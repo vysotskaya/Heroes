87bbceee32a6f20d5d00441a2b059c8da9551eea
Olga Vysotskaya <helga.vysotskaya@gmail.com>
2017-09-22 12:24:44 +0300

    routing

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit { 
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelected(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
